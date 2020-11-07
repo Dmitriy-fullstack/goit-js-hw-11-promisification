0b6864fbebe5d0bdba0c2f60ec534e6ee148f113
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-11-07 09:36:28 +0200

    fix task 2/1

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const delay = ms => {
   return new Promise((resolve, reject) => {
     const time = ms;
     setTimeout(() => {
-      resolve (ms = time);
-      
+      resolve(time)
+     
     }, ms)
   });
 };
@@ -16,3 +16,7 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+
+
+// Задание №2
